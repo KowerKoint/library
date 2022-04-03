b9b2744a2611246b2c73c152343e28bc62edf941
KowerKoint <kowerkoint@gmail.com>
2022-04-03 09:04:17 +0900

    add split

--- base.hpp
+++ base.hpp
@@ -96,6 +96,33 @@ istream &operator>>(istream &is, vector< T > &v) {
     return is;
 }
 
+template<typename T>
+vector<vector<T>> split(typename vector<T>::const_iterator begin, typename vector<T>::const_iterator end, T val) {
+    vector<vector<T>> res;
+    vector<T> cur;
+    for(auto it = begin; it != end; it++) {
+        if(*it == val) {
+            res.push_back(cur);
+            cur.clear();
+        } else cur.push_back(val);
+    }
+    res.push_back(cur);
+    return res;
+}
+
+vector<string> split(typename string::const_iterator begin, typename string::const_iterator end, char val) {
+    vector<string> res;
+    string cur = "";
+    for(auto it = begin; it != end; it++) {
+        if(*it == val) {
+            res.push_back(cur);
+            cur.clear();
+        } else cur.push_back(val);
+    }
+    res.push_back(cur);
+    return res;
+}
+
 template< typename T1, typename T2 >
 inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
 
