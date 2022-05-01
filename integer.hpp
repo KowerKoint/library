f3a90be91032d31e02b81c26dfcc4bf978ac3abe
KowerKoint <kowerkoint@gmail.com>
2022-05-01 19:00:50 +0900

    invmod, powmod

--- integer.hpp
+++ integer.hpp
@@ -76,3 +76,28 @@ ll extgcd(ll a, ll b, ll& x, ll& y) {
     }
     return a;
 }
+
+ll inv_mod(ll n, ll m) {
+    ll x, y;
+    assert(extgcd(n, m, x, y) == 1);
+    x %= m;
+    if(x < 0) x += m;
+    return x;
+}
+
+ll pow_mod(ll a, ll n, ll m) {
+    if(n == 0) return 1LL;
+    if(n < 0) return inv_mod(pow_mod(a, -n, m), m);
+    ll res = 1;
+    while(n) {
+        if(n & 1) {
+            res *= a;
+            res %= m;
+        }
+        n >>= 1;
+        a *= a;
+        a %= m;
+    }
+    return res;
+}
+
