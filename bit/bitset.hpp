d65584bd6fdc238db62b40bb30d7a5eb8705910d
KowerKoint <kowerkoint@gmail.com>
2022-08-30 15:14:57 +0900

    bitset bit operation

--- bit/bitset.hpp
+++ bit/bitset.hpp
@@ -1,39 +1,23 @@
 #include "../base.hpp"
 
 struct Bitset {
+    private:
+    void correct() {
+        for(int i = n - (bnum-1)*64; i < 64; i++) {
+            v[bnum-1] &= ~(1 << i);
+        }
+    }
+    public:
     vector<ull> v;
-    int n;
+    int n, bnum;
     Bitset(int n_ = 0) : n(n_) {
-        v.resize((n+63) / 64);
+        int bnum = (n+63) / 64;
+        v.resize(bnum);
     }
     int operator[](int i) {
         return (v[i/64] >> (i%64)) & 1;
     }
-    void set(int i) {
-        v[i / 64] |= 1ull << (i % 64);
-    }
-    void reset(int i) {
-        v[i / 64] &= ~(1ull << (i % 64));
-    }
-    void flip(int i) {
-        v[i / 64] ^= 1ull << (i % 64);
-    }
-    void resize(int n_) {
-        n = n_;
-        v.resize((n+63) / 64);
-    }
-    void all_set() {
-        fill(v.begin(), v.end(),  ~0ULL);
-    }
-    void all_reset() {
-        fill(v.begin(), v.end(), 0);
-    }
-    void all_flip() {
-        for (int i = 0; i < v.size(); i++) {
-            v[i] = ~v[i];
-        }
-    }
-    int count_all() {
+    int count() {
         int c = 0;
         for (int i = 0; i < v.size(); i++) {
             c += __builtin_popcountll(v[i]);
@@ -54,5 +38,102 @@ struct Bitset {
             for(int i = 0; i < r % 64; i++) c += (v[r2] >> i) & 1;
         }
         return c;
+    }
+    bool all() {
+        return count() == n;
+    }
+    bool any() {
+        return count() > 0;
+    }
+    bool none() {
+        return count() == 0;
+    }
+    void set(int i) {
+        v[i / 64] |= 1ull << (i % 64);
+    }
+    void reset(int i) {
+        v[i / 64] &= ~(1ull << (i % 64));
+    }
+    void flip(int i) {
+        v[i / 64] ^= 1ull << (i % 64);
+    }
+    void resize(int n_) {
+        n = n_;
+        v.resize((n+63) / 64);
+        correct();
+    }
+    void all_set() {
+        fill(v.begin(), v.end(),  ~0ULL);
+        correct();
+    }
+    void all_reset() {
+        fill(v.begin(), v.end(), 0);
+    }
+    void all_flip() {
+        for (int i = 0; i < v.size(); i++) {
+            v[i] = ~v[i];
+        }
+        correct();
+    }
+    Bitset& operator&=(const Bitset& b) {
+        for(int i = 0; i < min(bnum, b.bnum); i++) {
+            v[i] &= b.v[i];
+        }
+        return *this;
+    }
+    Bitset operator&(const Bitset& b) const {
+        return Bitset(*this) &= b;
+    }
+    Bitset& operator|=(const Bitset& b) {
+        for(int i = 0; i < min(bnum, b.bnum); i++) {
+            v[i] |= b.v[i];
+        }
+        correct();
+        return *this;
+    }
+    Bitset operator|(const Bitset& b) const {
+        return Bitset(*this) |= b;
+    }
+    Bitset& operator^=(const Bitset& b) {
+        for(int i = 0; i < min(bnum, b.bnum); i++) {
+            v[i] ^= b.v[i];
+        }
+        correct();
+        return *this;
+    }
+    Bitset operator^(const Bitset& b) const {
+        return Bitset(*this) ^= b;
+    }
+    Bitset operator~() const {
+        Bitset b(*this);
+        b.all_flip();
+        return b;
+    }
+    bool operator==(const Bitset& b) const {
+        return v == b.v;
+    }
+    bool operator!=(const Bitset& b) const {
+        return v != b.v;
+    }
+    Bitset& operator<<=(int sz) {
+        for(int i = bnum-1; i >= 0; i--) {
+            if(i-sz/64 >= 0) v[i] = v[i-sz/64] << (sz%64);
+            if(i-sz/64-1 >= 0) v[i] |= v[i-sz/64-1] >> (64-sz%64);
+        }
+        correct();
+        return *this;
+    }
+    Bitset operator<<(int sz) const {
+        return Bitset(*this) <<= sz;
+    }
+    Bitset& operator>>=(int sz) {
+        for(int i = 0; i < bnum; i++) {
+            if(i+sz/64 < bnum) v[i] = v[i+sz/64] >> (sz%64);
+            if(i+sz/64+1 < bnum) v[i] |= v[i+sz/64+1] << (64-sz%64);
+        }
+        return *this;
+    }
+    Bitset operator>>(int sz) const {
+        return Bitset(*this) >>= sz;
     }
 };
