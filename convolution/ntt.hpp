410c77d9638a0debe72eecd107b448abbdddd616
KowerKoint <kowerkoint@gmail.com>
2022-10-19 13:31:53 +0900

    ntt

---
+++ convolution/ntt.hpp
@@ -0,0 +1,70 @@
+#pragma once
+#include "integer/modint.hpp"
+
+template <ll (*mod)()>
+void ntt(vector<Modint<mod>>& v) {
+    assert(mod() == 998244353);
+    constexpr ll g = 3;
+    int n = v.size();
+    assert((n & (n - 1)) == 0);
+    int m = __builtin_ctz(n);
+    assert(m <= 23);
+    vector<Modint<mod>> zeta(n);
+    zeta[0] = 1;
+    Modint<mod> gn = Modint<mod>(g).pow((mod() - 1) >> m);
+    for(int i = 0; i < n-1; i++) zeta[i+1] = zeta[i] * gn;
+    int array_idx_mask = 0;
+    int array_id_mask = n-1;
+    for(int i = 0; i < m; i++) {
+        array_idx_mask ^= 1 << (m-i-1);
+        array_id_mask ^= 1 << (m-i-1);
+        vector<Modint<mod>> nv(n);
+        for(int j = 0; j < n; j++) {
+            int k = (((j & array_idx_mask) << 1) & array_idx_mask) | (j & array_id_mask);
+            nv[j] = v[k] + zeta[j & array_idx_mask] * v[k | (1 << (m-i-1))];
+        }
+        v.swap(nv);
+    }
+}
+
+template <ll (*mod)()>
+void intt(vector<Modint<mod>>& v) {
+    assert(mod() == 998244353);
+    constexpr ll ig = 332748118;
+    int n = v.size();
+    assert((n & (n - 1)) == 0);
+    int m = __builtin_ctz(n);
+    assert(m <= 23);
+    vector<Modint<mod>> izeta(n);
+    izeta[0] = 1;
+    Modint<mod> ign = Modint<mod>(ig).pow((mod() - 1) >> m);
+    for(int i = 0; i < n-1; i++) izeta[i+1] = izeta[i] * ign;
+    int array_idx_mask = 0;
+    int array_id_mask = n-1;
+    for(int i = 0; i < m; i++) {
+        array_idx_mask ^= 1 << (m-i-1);
+        array_id_mask ^= 1 << (m-i-1);
+        vector<Modint<mod>> nv(n);
+        for(int j = 0; j < n; j++) {
+            int k = (((j & array_idx_mask) << 1) & array_idx_mask) | (j & array_id_mask);
+            nv[j] = v[k] + izeta[j & array_idx_mask] * v[k | (1 << (m-i-1))];
+        }
+        v.swap(nv);
+    }
+    for(int i = 0; i < n; i++) v[i] /= n;
+}
+
+template <ll (*mod)()>
+vector<Modint<mod>> sum_convolution(const vector<Modint<mod>>& v1, const vector<Modint<mod>>& v2) {
+    assert(mod() == 998244353);
+    int n = 1;
+    while(n < (int)v1.size() + (int)v2.size() - 1) n <<= 1;
+    vector<Modint<mod>> f1(v1), f2(v2);
+    f1.resize(n); f2.resize(n);
+    ntt(f1); ntt(f2);
+    for(int i = 0; i < n; i++) {
+        f1[i] *= f2[i];
+    }
+    intt(f1);
+    return f1;
+}
---
+++ test/yosupo-convolution.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+#include "convolution/ntt.hpp"
+
+int main() {
+    int n, m; cin >> n >> m;
+    vector<MI3> a(n), b(m); cin >> a >> b;
+    vector<MI3> ans = sum_convolution(a, b);
+    ans.resize(n + m - 1);
+    print(ans);
+}
