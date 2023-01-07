623d0e51c0b5161155b02b5d1a5d25dd2e68341b
KowerKoint <kowerkoint@gmail.com>
2023-01-07 01:58:38 +0000

    iterator for segmentset, fix get()

--- segtree/segment-set.hpp
+++ segtree/segment-set.hpp
@@ -5,19 +5,27 @@ template <typename T, T neighbor=1>
 struct SegmentSet {
     map<T, T> segments;
     using It = typename map<T, T>::iterator;
+    using CIt = typename map<T, T>::const_iterator;
 
-    It get(T x) const {
-        auto it = segments.upper_bound(x);
+    It begin() { return segments.begin(); }
+    It end() { return segments.end(); }
+    CIt begin() const { return segments.begin(); }
+    CIt end() const { return segments.end(); }
+
+    CIt get(T x) const {
+        CIt it = segments.upper_bound(x);
         if (it == segments.begin()) return segments.end();
-        return prev(it);
+        it--;
+        if (it->first <= x && x <= it->second) return it;
+        return segments.end();
     }
 
     bool contains(T x) const {
         return get(x) != segments.end();
     }
 
-    It insert(T l, T r) {
-        auto it = segments.upper_bound(l);
+    CIt insert(T l, T r) {
+        It it = segments.upper_bound(l);
         if(it != segments.begin() && prev(it)->second + neighbor >= l) {
             it = prev(it);
             l = it->first;
@@ -26,12 +34,12 @@ struct SegmentSet {
             r = max(r, it->second);
             it = segments.erase(it);
         }
-        return segments.emplace_hint(it, l, r);
+        return segments.emplace(l, r).first;
     }
 
     // not verified
     void erase(T l, T r) {
-        auto it = segments.upper_bound(l);
+        It it = segments.upper_bound(l);
         if(it != segments.begin() && prev(it)->second + neighbor > l) {
             it = prev(it);
         }
@@ -47,3 +55,4 @@ struct SegmentSet {
         }
     }
 };
+
